5d27c101f7a71d21c40b1e3324bc71b5fb9497cd
Trần Lê Quang Anh <bestsurgamers@gmail.com>
2026-04-22 21:35:42 +0700

    Add files via upload

--- baitapbai24.cpp
+++ baitapbai24.cpp
@@ -12,17 +12,14 @@ Output
 Vị trí của con ếch sau k bước nhảy.*/
 int main(){
     int a, b, k;
-    start:
-    scanf("%d %d %d", &a, &b, &k);
-
-    if ((a<0 || a>1e9) || (b<0 || b>1e9) || (k<0 || k>1e9)){
-        printf("INVALID, INPUT AGAIN: ");
-        goto start;
-    } else {
-        long long t=1ll*(k/2);
-        long long p=1ll*(k/2)+1ll*(k%2);
-        long long x=p*a-t*b;
-        printf("%lld\n", x);
-    }
+    do{
+        scanf("%d %d %d", &a, &b, &k);
+        if ((a<0 || a>1e9) || (b<0 || b>1e9) || (k<0 || k>1e9)) printf("INVALID, INPUT AGAIN: ");
+    } while ((a<0 || a>1e9) || (b<0 || b>1e9) || (k<0 || k>1e9));
+    long long t=1ll*(k/2);
+    long long p=1ll*(k/2)+1ll*(k%2);
+    long long x=p*a-t*b;
+    printf("%lld\n", x);
+        
     return 0;
 }
